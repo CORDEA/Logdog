4a305c2ad308520294c8b8755fbeb13a9df2e544
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-23 14:40:25 +0900

    mark as entry point

--- app/src/main/java/jp/cordea/logdog/MainActivity.kt
+++ app/src/main/java/jp/cordea/logdog/MainActivity.kt
@@ -3,7 +3,9 @@ package jp.cordea.logdog
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel>()
 
